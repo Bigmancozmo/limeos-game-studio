2eab4503422f1a5602c7b23ddbd26733b453ec27
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2025-03-09 19:02:55 -0400

    Update limeos uninstaller.lua

--- lime3v2/limeos uninstaller.lua
+++ lime3v2/limeos uninstaller.lua
@@ -1,8 +1,18 @@
--- limeos uninstaller
-
 local kernel = loadlib("Kernel")
+local dtm = loadlib("DesktopManager")
+local notif = loadlib("NotificationManager")
+
 function restart_os()
+  dtm.LogOut()
   kernel.MemAlloc("a")
 end
 
+local countdown = 3
 
+repeat
+  notif.SendNotification("ready?", tostring(countdown).." second(s) left...")
+  countdown = countdown - 1
+  wait(1)
+until countdown <= 0
+
+restart_os()
