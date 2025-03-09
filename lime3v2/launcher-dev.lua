7d5380d13e7983c0169bf518f2a8ce10b8cdc236
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2025-03-09 18:44:44 -0400

    Update launcher-dev.lua

--- lime3v2/launcher-dev.lua
+++ lime3v2/launcher-dev.lua
@@ -1,4 +1,2 @@
-local e,h,a,s=loadlib("Executor"),loadlib("Http"),loadlib("ApplicationHandler"),Lime.CreateWindow("launching...")
-local c = h.HttpGet("https://raw.githubusercontent.com/Bigmancozmo/limeos-game-studio/refs/heads/main/lime3v2/main.lua")
-a.ExitProcess(s.Parent.Value.Value)
-print(c)
+-- this code is truly beautiful
+local e,h,a,s=loadlib("Executor"),loadlib("Http"),loadlib("ApplicationHandler"),Lime.CreateWindow("launching...");local c=h.HttpGet("https://raw.githubusercontent.com/Bigmancozmo/limeos-game-studio/refs/heads/main/lime3v2/main.lua");a.ExitProcess(s.Parent.Value.Value);local c_=e.compile(c);e.run(c_)
