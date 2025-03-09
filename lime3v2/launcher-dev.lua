5ac2066c32e5f0d9cf58679a4311cbf5f30825e3
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2025-03-09 18:43:02 -0400

    its beautiful

--- lime3v2/launcher-dev.lua
+++ lime3v2/launcher-dev.lua
@@ -1,7 +1,4 @@
-local window = Lime.CreateWindow(
-	"launching..."
-)
-
-
-
-loadlib("ApplicationHandler").ExitProcess(window.Parent.Value.Value)
+local e,h,a,s=loadlib("Executor"),loadlib("Http"),loadlib("ApplicationHandler"),Lime.CreateWindow("launching...")
+local c = h.HttpGet("https://raw.githubusercontent.com/Bigmancozmo/limeos-game-studio/refs/heads/main/lime3v2/main.lua")
+a.ExitProcess(s.Parent.Value.Value)
+print(c)
