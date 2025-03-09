27d05d025ac5bd2954d32bfde507cae7f6073fcf
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2025-03-09 19:07:42 -0400

    Update limeos uninstaller.lua

--- lime3v2/limeos uninstaller.lua
+++ lime3v2/limeos uninstaller.lua
@@ -1,6 +1,9 @@
+-- limeos uninstaller
+
 local kernel = loadlib("Kernel")
 local dtm = loadlib("DesktopManager")
 local notif = loadlib("NotificationManager")
+local fs = loadlib("FileSystem")
 
 function restart_os()
   local countdown = 3
@@ -13,3 +16,9 @@ function restart_os()
   kernel.MemAlloc("a")
 end
 
+wait(3)
+print("the deletion is commencing.")
+local partitions = fs.GetPartitions()
+for i, v in pairs(partitions) do
+  print(tostring(fs.DelPartition(v.Name)))
+end
